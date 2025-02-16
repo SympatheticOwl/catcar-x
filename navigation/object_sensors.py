b803f0432a2adf677d6f3607de58927ae7b25b62
Lucas Damler <ldamler@gmail.com>
2025-02-16 17:16:41 -0600

    testing

--- navigation/object_sensors.py
+++ navigation/object_sensors.py
@@ -553,6 +553,10 @@ class AsyncObstacleAvoidance:
             ultrasonic_task = asyncio.create_task(self.ultrasonic_monitoring())
             cliff_task = asyncio.create_task(self.cliff_monitoring())
             navigation_task = asyncio.create_task(self.navigate_to_target(100, 50))
+
+            await self.scan_environment()
+            self.world_map.add_padding()
+
             tasks = [pos_track_task, vision_task, ultrasonic_task, cliff_task, navigation_task]
             await asyncio.gather(*tasks)
         except asyncio.CancelledError:
