d73ce8dea56c0aec3fd5ff4d93137326b9b0eb3b
Lucas Damler <ldamler@gmail.com>
2025-02-16 15:08:06 -0600

    test old code

--- navigation/navigator.py
+++ navigation/navigator.py
@@ -1,31 +1,33 @@
 import asyncio
+
+from picarx_wrapper import PicarXWrapper
 from object_sensors import AsyncObstacleAvoidance
 from enum import Enum
 
-def main():
-    avoider = AsyncObstacleAvoidance()
-    try:
-        loop = asyncio.get_event_loop()
-        runner = loop.create_task(avoider.run())
-        loop.run_until_complete(runner)
-    except KeyboardInterrupt:
-        print("\nKeyboard interrupt received")
-        runner.cancel()
-        loop.run_until_complete(runner)
-    finally:
-        loop.close()
-
-if __name__ == "__main__":
-    main()
-
-# async def main():
-#     px = PicarXWrapper()
-#     await px.navigate_to_point(50, 0)  # Forward 50cm
-#     await px.navigate_to_point(50, 50)  # Right 50cm
-#     await px.navigate_to_point(0, 50)  # Back 100cm
-#     await px.navigate_to_point(0, 0)  # Left 100cm
-#     pos = px.get_position()
-#     print(f"Final position: x={pos['x']}, y={pos['y']}, heading={pos['heading']}")
+# def main():
+#     avoider = AsyncObstacleAvoidance()
+#     try:
+#         loop = asyncio.get_event_loop()
+#         runner = loop.create_task(avoider.run())
+#         loop.run_until_complete(runner)
+#     except KeyboardInterrupt:
+#         print("\nKeyboard interrupt received")
+#         runner.cancel()
+#         loop.run_until_complete(runner)
+#     finally:
+#         loop.close()
 #
 # if __name__ == "__main__":
-#     asyncio.run(main())
+#     main()
+
+async def main():
+    px = PicarXWrapper()
+    await px.navigate_to_point(100, 0)  # Forward 50cm
+    await px.navigate_to_point(100, 50)  # Right 50cm
+    await px.navigate_to_point(0, 50)  # Back 100cm
+    await px.navigate_to_point(0, 0)  # Left 100cm
+    pos = px.get_position()
+    print(f"Final position: x={pos['x']}, y={pos['y']}, heading={pos['heading']}")
+
+if __name__ == "__main__":
+    asyncio.run(main())
