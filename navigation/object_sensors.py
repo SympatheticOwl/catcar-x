d43de9c02de83562276b409fbed45178a7b0a236
Lucas Damler <ldamler@gmail.com>
2025-02-16 15:36:55 -0600

    remove init scan

--- navigation/object_sensors.py
+++ navigation/object_sensors.py
@@ -321,12 +321,6 @@ class AsyncObstacleAvoidance:
             vision_task = asyncio.create_task(self.vision.capture_and_detect())
             ultrasonic_task = asyncio.create_task(self.ultrasonic_monitoring())
             cliff_task = asyncio.create_task(self.cliff_monitoring())
-
-            # Initial environment scan
-            print("Performing initial environment scan...")
-            await self.scan_environment()
-            self.world_map.add_padding()
-
             navigation_task = asyncio.create_task(self.navigate_to_goal(100, 50))
             tasks = [pos_track_task, vision_task, ultrasonic_task, cliff_task, navigation_task]
             await asyncio.gather(*tasks)
