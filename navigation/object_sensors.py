18359dfc3d8af80f0325ade69dafaaf7c1b1bd00
Lucas Damler <ldamler@gmail.com>
2025-02-16 15:13:01 -0600

    revert

--- navigation/object_sensors.py
+++ navigation/object_sensors.py
@@ -384,21 +384,9 @@ class AsyncObstacleAvoidance:
             ultrasonic_task = asyncio.create_task(self.ultrasonic_monitoring())
             cliff_task = asyncio.create_task(self.cliff_monitoring())
             # movement_task = asyncio.create_task(self.forward_movement())
-            navigation_task = asyncio.create_task(self.px.navigate_to_point(100, 0))
-            tasks = [pos_track_task, vision_task, ultrasonic_task, cliff_task, navigation_task]
-            await asyncio.gather(navigation_task)
-
             navigation_task = asyncio.create_task(self.px.navigate_to_point(100, 50))
             tasks = [pos_track_task, vision_task, ultrasonic_task, cliff_task, navigation_task]
-            await asyncio.gather(navigation_task)
-
-            navigation_task = asyncio.create_task(self.px.navigate_to_point(0, 50))
-            tasks = [pos_track_task, vision_task, ultrasonic_task, cliff_task, navigation_task]
-            await asyncio.gather(navigation_task)
-
-            navigation_task = asyncio.create_task(self.px.navigate_to_point(0, 0))
-            tasks = [pos_track_task, vision_task, ultrasonic_task, cliff_task, navigation_task]
-            await asyncio.gather(navigation_task)
+            await asyncio.gather(*tasks)
         except asyncio.CancelledError:
             print("\nShutting down gracefully...")
         finally:
