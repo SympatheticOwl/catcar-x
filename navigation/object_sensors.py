6f6938015d7ab50b6e389d76367ea818849171d0
Lucas Damler <ldamler@gmail.com>
2025-02-16 22:22:20 -0600

    revert

--- navigation/object_sensors.py
+++ navigation/object_sensors.py
@@ -293,7 +293,7 @@ class AsyncObstacleAvoidance:
             vision_task = asyncio.create_task(self.vision.capture_and_detect())
             ultrasonic_task = asyncio.create_task(self.ultrasonic_monitoring())
             cliff_task = asyncio.create_task(self.cliff_monitoring())
-            navigation_task = asyncio.create_task(self.navigate_to_point(100, 50))
+            navigation_task = asyncio.create_task(self.navigate_to_point(100, 100))
 
             tasks = [
                 pos_track_task,
