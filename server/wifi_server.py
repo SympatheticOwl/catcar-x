f4ee93a0d792b9b9475086402ef59eaf9b81b545
Lucas Damler <ldamler@gmail.com>
2025-02-20 19:02:37 -0600

    testing

--- server/wifi_server.py
+++ server/wifi_server.py
@@ -16,7 +16,7 @@ with (socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s):
                 client, clientInfo = s.accept()
                 print("server recv from: ", clientInfo)
                 data = client.recv(1024)      # receive 1024 Bytes of message in binary format
-                if decoded_data != b"":
+                if data != b"":
                     print(f"Received: {data}")
                     decoded_data = data.decode('utf-8').strip()
                     if decoded_data == 'forward':
